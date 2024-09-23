2528ee75ce316af1716ac36597907b4ad4f30e45
Chris <4knowin9@gmail.com>
2024-09-23 08:58:02 -0300

    Website blockout

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='flex flex-col gap-3 w-screen h-screen'>
+      <div className='bg-[#1b1b1b] top-0 h-10'>Month Navbar</div>
+      <p>Month</p>
+      <div className='flex flex-row gap-3 h-2/5'>
+        <div className='bg-[#1b1b1b] w-1/2'>Graph</div>
+        <div className='bg-[#1b1b1b] w-[30%]'>Pie Chart</div>
+        <div className='bg-[#1b1b1b] w-[20%]'>Remaining Balance</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='flex flex-col gap-3 bg-[#1b1b1b] h-1/2'>
+        Expenses list
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
