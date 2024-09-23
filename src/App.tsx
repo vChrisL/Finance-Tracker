2f6b48ed199b02e1ab65ccd8d8f82bfc0ca3764c
Chris <4knowin9@gmail.com>
2024-09-23 12:01:22 -0300

    added navbar month btns

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,21 @@ function App() {
   return (
     <>
     <div className='flex flex-col gap-3 w-screen h-screen px-3 pb-3'>
-      <div className='bg-[#ffffff] shadow-md shadow-[#b6b6b6] top-0 h-10 rounded-b-xl'>Month Navbar</div>
+      <div className='flex flex-row justify-center gap-14 bg-[#ffffff] shadow-md shadow-[#b6b6b6] top-0 h-10 rounded-b-xl'>
+        <button>Jan</button>
+        <button>Feb</button>
+        <button>Mar</button>
+        <button>Apr</button>
+        <button>May</button>
+        <button>Jun</button>
+        <button>Jul</button>
+        <button>Aug</button>
+        <button>Sep</button>
+        <button>Oct</button>
+        <button>Nov</button>
+        <button>Dec</button>
+        <button>Yearly</button>
+      </div>
       <p className='text-2xl font-semibold'>Month</p>
       <div className='flex flex-row gap-3 h-2/5'>
         <div className='bg-[#ffffff] shadow-md shadow-[#b6b6b6] w-1/2 rounded-xl'>Graph</div>
