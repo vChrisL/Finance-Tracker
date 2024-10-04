854af43bf744a07a5aa9e79c96c3c55e9d9f761b
Chris <4knowin9@gmail.com>
2024-10-04 08:57:41 -0300

    replaced div in favor of form for new expense menu

--- src/components/NewExpenseMenu-component.tsx
+++ src/components/NewExpenseMenu-component.tsx
@@ -69,7 +69,7 @@ export function NewExpenseMenu() {
     <>
       <div className="flex justify-center absolute top-0 right-0 w-screen h-screen backdrop-blur-sm bg-[#b6b6b648]">
         <div className="flex flex-col justify-around items-center p-6 bg-[#ffffff] shadow-md shadow-[#b6b6b6] w-fit h-fit top-0 right-0 translate-y-1/2 rounded-xl">
-          {/* <form className="flex flex-col justify-center items-center"> */}
+          <form onSubmit={newExpense} className="flex flex-col justify-center items-center">
             <div className="flex flex-row gap-4">
               <div className="flex flex-col gap-4 text-right font-semibold">
                 <p>Description:</p>
@@ -110,9 +110,9 @@ export function NewExpenseMenu() {
 
             <div className="flex flex-row gap-8 mt-4">
               <button className="hover:text-red-500" onClick={() => setMenuState(!menuState)}>Discard</button>
-              <button onClick={newExpense}>Add</button>
+              <button type="submit">Add</button>
             </div>
-          {/* </form> */}
+          </form>
         </div>
       </div>
     </>
