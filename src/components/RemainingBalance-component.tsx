83418a2245e9176ff9293a2ade39502263890e16
Chris <4knowin9@gmail.com>
2024-10-05 12:24:58 -0300

    added basic remaining balance component

--- src/components/RemainingBalance-component.tsx
+++ src/components/RemainingBalance-component.tsx
@@ -1,7 +1,27 @@
-import { useExpensesList, expenseCategories } from "../data/expenseData";
+import { useExpensesList, expenseCategories, useMonthStore } from "../data/expenseData";
 
 export function RemainingBalance(){
-    const masterExpenseList = useExpensesList(state => state.storeExpenses)
+    // use storeExpenses store and selectedMonth store
+    const masterExpenseList = useExpensesList(state => state.storeExpenses);
+    const selectedMonth = useMonthStore(state => state.selectedMonth);
+
+    // number format for currency
+    const numberFormat = Intl.NumberFormat("en-CA", {style: "currency", currency: 'CAD'})
+
+    // Handles getting the total for each category
+    function getCategoryTotalSpending(targetCategory: string): number{
+        let tempTotal: number = 0;
+
+        // for each expense in the master expense list
+        masterExpenseList.forEach(expense => {
+            // if expense category is equal to the target category; add it's value to the total
+            if(expense.category === targetCategory && expense.month === selectedMonth){
+                tempTotal += expense.amount;
+            }
+        });
+
+        return tempTotal;
+    }
 
     return(
         <>
@@ -10,7 +30,7 @@ export function RemainingBalance(){
 
             <ul className="">
                 {expenseCategories.map((category) => 
-                    <p>{category}: $$$</p>
+                    <p>{category}: {numberFormat.format(getCategoryTotalSpending(category))}</p>
                 )}
             </ul>
         </div>
